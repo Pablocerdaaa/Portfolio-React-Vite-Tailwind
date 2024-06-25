23a5fb206fb8450341f4c5d6c591cd653da4a1eb
Pablocerdaaa <pablo.cerda@alumnos.ucn.cl>
2024-06-25 12:58:32 -0400

    added navbar and css code
    added navbar and css code

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,12 +1,20 @@
-
-
-function App() {
-
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+import { NavbarSimple } from "./components/Navbar.jsx";
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <>
+    <NavbarSimple/>
+    <h1 className="text-3xl font-bold underline"> Pablo Cerda </h1>
+
+    <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+      <Button className="mt-10" href="#login">
+        algo
+      </Button>
+    </div>
+    <ButtonGradient/>
+    </>
+  );
+};
 
 export default App
