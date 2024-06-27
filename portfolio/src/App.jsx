e804815845d53b1557dece17a7035e1d812b6afc
Pablocerdaaa <pablo.cerda@alumnos.ucn.cl>
2024-06-27 17:17:35 -0400

    added another wallpaper

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,18 +1,15 @@
-// src/App.jsx
-
 import React from "react";
 import Navbar from './components/Navbar';
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Button from "./components/Button";
 import Card from "./components/Card";
 import backgroundSM from "./assets/background/sobre-mi.jpg";
+import backgroundEst from "./assets/background/estudios.jpg";
 
 const App = () => {
   return (
-    <div
-      className="absolute inset-0 bg-cover bg-center "
-      style={{backgroundImage: `url(${backgroundSM})`}}
-    >
+    <div className="relative min-h-screen">
+      <div className="absolute inset-0" style={{backgroundImage: `url(${backgroundSM})`, zIndex: -1}} />
       <Navbar />
       <h1 className="text-3xl font-bold underline p-10 text-center">Pablo Cerda Isasmendi</h1>
       <div className="flex justify-center">
@@ -21,13 +18,14 @@ const App = () => {
           description="Soy Pablo Cerda Isasmendi, nacido en Chillán y actualmente residiendo en Antofagasta. Me titulé en Ingeniería en Computación e Informática en 2024. Estoy expandiendo mis conocimientos en desarrollo web, tanto en backend como en frontend, con un enfoque particular en React y Tailwind CSS.
 
 Además, tengo experiencia en la evaluación de proyectos TI y data science. Estoy entusiasmado por adquirir más conocimientos y experiencia profesional en estos campos, siempre buscando nuevos desafíos y oportunidades para crecer y mejorar mis habilidades."
-          backgroundImage="/assets/background/sobre-mi.jpg" // Esta línea es opcional según cómo estés usando la imagen en el componente Card
         />
       </div>
+      <div className="p-96"></div>
       <footer className="bg-gray-900 text-white py-6 text-center absolute bottom-0 w-full">
         <p>&copy; 2024 Pablo Cerda Isasmendi. Todos los derechos reservados.</p>
       </footer>
     </div>
+    
   );
 };
 
--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,28 +1,27 @@
-import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
-import Pclogo from "../assets/svg/PC.svg"
+import React from "react";
+import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/react';
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import Pclogo from "../assets/svg/PC.svg";
 
 const navigation = [
   { name: 'Sobre mi', href: '#', current: true },
   { name: 'Proyectos', href: '#', current: false },
   { name: 'Información de contacto', href: '#', current: false },
-]
+];
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(' ');
 }
 
 export default function Navbar() {
   return (
-    <Disclosure as="nav" className="bg-gray-800">
+    <Disclosure as="nav" className="bg-gray-800 sticky top-0 z-50">
       {({ open }) => (
         <>
-          <div className="mx-auto sm:px-6 lg:px-8">
-            <div className="relative flex h-16 items-center justify-between">
+          <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+            <div className="relative flex items-center justify-between h-16">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button*/}
-                <DisclosureButton className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="absolute -inset-0.5" />
+                <DisclosureButton className="inline-flex items-center justify-center p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
@@ -31,15 +30,15 @@ export default function Navbar() {
                   )}
                 </DisclosureButton>
               </div>
-              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex flex-shrink-0 items-center">
+              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex-shrink-0">
                   <img
-                    className="h-10 w-auto rounded-full  border-4 border-yellow-500"
+                    className="h-10 w-auto rounded-full border-4 border-yellow-500"
                     src={Pclogo}
                     alt="Your Company"
                   />
                 </div>
-                <div className="hidden sm:ml-6 sm:block">
+                <div className="hidden sm:block sm:ml-6">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
                       <a
@@ -47,7 +46,7 @@ export default function Navbar() {
                         href={item.href}
                         className={classNames(
                           item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                          'rounded-md px-3 py-2 text-sm font-medium','right-1'
+                          'rounded-md px-3 py-2 text-sm font-medium',
                         )}
                         aria-current={item.current ? 'page' : undefined}
                       >
@@ -62,17 +61,15 @@ export default function Navbar() {
                   type="button"
                   className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                 >
-                  <span className="absolute -inset-1.5" />
                   <span className="sr-only">View notifications</span>
                   <BellIcon className="h-6 w-6" aria-hidden="true" />
                 </button>
-
               </div>
             </div>
           </div>
 
           <DisclosurePanel className="sm:hidden">
-            <div className="space-y-1 px-2 pb-3 pt-2">
+            <div className="px-2 pt-2 pb-3 space-y-1">
               {navigation.map((item) => (
                 <DisclosureButton
                   key={item.name}
@@ -92,5 +89,5 @@ export default function Navbar() {
         </>
       )}
     </Disclosure>
-  )
+  );
 }
