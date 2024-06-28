ec5d7954099ec26d7008a196e654b1298b230bc2
Pablocerdaaa <pablo.cerda@alumnos.ucn.cl>
2024-06-27 22:59:31 -0400

    changinf background

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import Navbar from './components/Navbar';
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Button from "./components/Button";
-import Card from "./components/Card";
+import Card from "./components/Cards/Card";
 import backgroundSM from "./assets/background/sobre-mi.jpg";
-import backgroundEst from "./assets/background/estudios.jpg";
+import backgroundEst from "./assets/background/estudios2.jpg";
 
 const App = () => {
   return (
@@ -15,21 +15,26 @@ const App = () => {
       {/* Contenido principal */}
       <div className="relative z-10">
         <Navbar />
-        <h1 className="text-3xl font-bold underline p-10 text-center">Pablo Cerda Isasmendi</h1>
-        <div className="flex justify-center">
-          <Card
+          <div className="flex justify-center items-center h-full">
+            <h1 className="text-3xl font-bold underline p-10 text-center">Pablo Cerda Isasmendi</h1>
+          </div>
+          <div className="flex">
+            <div className="relative w-full h-screen bg-cover bg-center" style={{ backgroundImage: `url(${backgroundSM})` }}>
+              <div className="flex justify-center items-center h-full">
+            <Card
             title="Sobre mí"
             description="Soy Pablo Cerda Isasmendi, nacido en Chillán y actualmente residiendo en Antofagasta. Me titulé en Ingeniería en Computación e Informática en 2024. Estoy expandiendo mis conocimientos en desarrollo web, tanto en backend como en frontend, con un enfoque particular en React y Tailwind CSS.
 
 Además, tengo experiencia en la evaluación de proyectos TI y data science. Estoy entusiasmado por adquirir más conocimientos y experiencia profesional en estos campos, siempre buscando nuevos desafíos y oportunidades para crecer y mejorar mis habilidades."
           />
-        </div>
-        <div className="p-96"></div>
+              </div>
+            </div>
+          </div>
         
         {/* Nueva sección con otro fondo */}
         <div className="relative w-full h-screen bg-cover bg-center" style={{ backgroundImage: `url(${backgroundEst})` }}>
           <div className="flex justify-center items-center h-full">
-            <h2 className="text-3xl font-bold text-white">Sección de Estudios</h2>
+            <Card title="Estudios" description="Escuela Huanchaca E-56" />
           </div>
         </div>
         
